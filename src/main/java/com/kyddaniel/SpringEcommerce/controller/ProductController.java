a68da5430a447ce1e395dce82e18c5c8ab114754
kyddaniel <mksuenaa@connect.ust.hk>
2024-12-03 22:02:23 +0800

    Implement method add product with image

--- src/main/java/com/kyddaniel/SpringEcommerce/controller/ProductController.java
+++ src/main/java/com/kyddaniel/SpringEcommerce/controller/ProductController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -31,5 +33,19 @@ public class ProductController {
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestBody Product product, @RequestPart MultipartFile imageFile) {
+        Product savedProduct = null;
+        try {
+            savedProduct = productService.addProduct(product, imageFile);
+            return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+        }
+        catch (IOException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+
     }
 }
